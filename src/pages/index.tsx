dd3825f4380bd4f84add28aca0803154377b5fa0
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-01 18:30:03 -0300

    Get info from GitHub

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-export default function Home() {
-  return (
-    <h1>Hello</h1>
-  )
+import {GetStaticProps} from "next";
+
+export default function Home({user}) {
+    return (
+        <div>
+            <h1>{user.login}</h1>
+            <h3>{user.bio}</h3>
+
+            <p>Site: <a href={user.blog}>{user.blog}</a></p>
+        </div>
+    )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const response = await fetch(`https://api.github.com/users/igorlrnc`)
+    const data = await response.json()
+
+    return {
+        props: {
+            user: data
+        }, revalidate: 10
+    }
+};
